315d686ecbcdb06f609f1fe174ad309c2fce7804
Shelpec <se400045@gmail.com>
2024-02-08 09:11:23 +0600

    two comit

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,6 +1,13 @@
+import org.graalvm.compiler.lir.LIRInstruction;
+
 public class User {
     private String name;
     private int age;
+
+    public User(){
+        name = "dd";
+        age = 18;
+    }
 
     public int getAge() {
         return age;
